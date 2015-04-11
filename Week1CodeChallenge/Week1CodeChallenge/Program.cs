82fdcbcb43236e0857cdcdfb1f0b0bc2b4db4131
Colton Miller <ColtonJMMiller@gmail.com>
2015-04-11 11:00:58 -0600

    Started text stat and bonus yoda

--- Week1CodeChallenge/Week1CodeChallenge/Program.cs
+++ Week1CodeChallenge/Week1CodeChallenge/Program.cs
@@ -36,13 +36,35 @@ namespace Week1CodeChallenge
         {
             string[] yodaArray = text.Split(' ');
             Array.Reverse(yodaArray);
+            if (yodaArray.Length == 3)
+            {
+                string[] tempArray   
+            }
             string yodaTalk = string.Join(" ", yodaArray);
             return yodaTalk;
         }
         public static void TextStats(string input)
         {
+            int counter = 0;
 
+            for (int i = 0; i < input.Length; i++)
+            {
+                counter++;
+            }
+            Console.WriteLine("Number of characters: {0}", counter);
+            
+            counter = 0;
+            string[] words = input.Split(' ');
+            for (int i = 0; i < words.Length ; i++)
+            {
+                counter++;
+            }
+            Console.WriteLine("Number of words: {0}", counter);
 
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] =
+            }
         }
         public static bool IsPrime(int number)
         {
